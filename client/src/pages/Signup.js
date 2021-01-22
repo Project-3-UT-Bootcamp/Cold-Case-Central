a609230a8fec0ec613a6aaa3841aa760b73b4d64
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-21 20:48:55 -0600

    handleChange event listener

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,22 +1,31 @@
 import React, { useState } from "react";
 import { ADD_USER } from "../utils/mutation";
-import { useMutation } from '@apollo/react-hooks';
+import { useMutation } from "@apollo/react-hooks";
 
 function Signup() {
-    const [formState, setFormState] = useState(
-        { email: '', password: ''});
-    const [addUser] = useMutation(ADD_USER);
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [addUser] = useMutation(ADD_USER);
 
-    // 'submit' event listener
-    const handleFormSubmit = async event => {
-        event.preventDefault();
-        const mutationResponse = await addUser({
-            variables: {
-                email: formState.email,
-                password: formState.password,
-                firstName: formState.firstName,
-                lastName: formState.lastName
-            }
-        });
-    }
+  // 'submit' event listener
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName,
+      },
+    });
+  };
+
+  const handleChange = event => {
+      const { name, value } = event.target;
+      setFormState({
+          ...formState,
+          [name]: value
+      });
+  };
+
+  
 }
