dd4a466c3775b7ed61d213dcc3d13e9bab55f7e4
catse2000 <catse2000@yahoo.com>
2021-01-21 18:31:27 -0600

    server is working

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
-// Connect to server
+// import express
 const express = require('express');
 
+// test info
+const { animals } = require('./data/animals.json');
+
+// define local port
+const PORT = process.env.PORT || 3001;
+
+// instantiate express server
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.get('/api/animals', (req, res) => {
+    res.send('Hello!');
+})
+
+// connect to server
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 })
