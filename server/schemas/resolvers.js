e641b480ea839ae9da1b64dd6c73016f684bf80b
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-21 22:47:38 -0600

    adduser resovler

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,17 @@
 // imports
 
-// resolvers
+// resolvers - incomplete
 const resolvers = {
   Query: {},
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      return user;
+    },
+    login: async () => {
+
+    }
+  }
 };
 
 // exports
