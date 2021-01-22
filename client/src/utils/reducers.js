aed9f31591060ac295defba68d87ae6db51c6c9d
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-21 22:26:09 -0600

    imported actions

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,6 +1,10 @@
 import { useReducer } from "react";
 
 // IMPORT ACTIONS here once created
+import {
+    UPDATE_CATEGORIES,
+    UPDATE_POST
+} from './actions';
 
 // incomplete reducer
 export const reducer = (state, action) => {
