34f5fadbb8e0257175075b3e4a7cd7122fe0d014
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-21 21:13:10 -0600

    signup Function JSX

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -19,13 +19,64 @@ function Signup() {
     });
   };
 
-  const handleChange = event => {
-      const { name, value } = event.target;
-      setFormState({
-          ...formState,
-          [name]: value
-      });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
   };
 
-  
+  return (
+    <div className="">
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            placeholder="First"
+            name="firstName"
+            type="firstName"
+            id="firstName"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            placeholder="Last"
+            name="lastName"
+            type="lastName"
+            id="lastName"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
+
+export default Signup;
