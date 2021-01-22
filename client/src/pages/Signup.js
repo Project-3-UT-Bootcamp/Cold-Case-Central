c90e53d053edd1d646f90916959dc17f38e5d616
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-21 20:33:52 -0600

    signup function, imports

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,1 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { ADD_USER } from "../utils/mutation";
+import { useMutation } from '@apollo/react-hooks';
+
+function Signup() {
+    const [formState, setFormState] = useState(
+        { email: '', password: ''});
+    const [addUser] = useMutation(ADD_USER);
+}
