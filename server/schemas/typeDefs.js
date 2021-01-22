4668a38a6b7620641fbc44db0ff96f65bbcc030d
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-21 22:57:58 -0600

    updated login and addUser mutations to return Auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,8 +17,8 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        login(email: String!, password: String!): User
-        addUser(username: String!, email: String!, password: String!): User
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
     }
 
     type Auth {
