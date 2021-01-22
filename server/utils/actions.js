c91c09827d1a30f0b0589ee8a1cf300e53f5c094
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-21 19:24:22 -0600

    initialized files, added actions

---
+++ server/utils/actions.js
@@ -0,0 +1,2 @@
+export const UPDATE_POST = "UPDATE_POST";
+export const UPDATE_CATEGORIES = 'UPDATE_CATEGORIES';
--- server/utils/reducers.js
+++ server/utils/reducers.js
@@ -1,1 +1,7 @@
 import { useReducer } from "react";
+
+// IMPORT ACTIONS here once created
+
+// incomplete reducer
+export const reducer = (state, action) => {
+    switch (action.type)
