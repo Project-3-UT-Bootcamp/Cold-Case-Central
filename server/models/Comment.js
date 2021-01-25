6bcf657f0bb14eb5d1d27cc8ce99ff7dc39c7237
catse2000 <catse2000@yahoo.com>
2021-01-24 20:59:46 -0600

    updated user and comment models

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -5,7 +5,7 @@ const commentSchema = new Schema(
     {
         commentText: {
             type: String,
-            minlength: 1,
+            required: true,
             maxlength: 280
         },
         createdAt: {
@@ -20,8 +20,5 @@ const commentSchema = new Schema(
     }
 );
 
-// create the Comment model using the commentSchema
-const Comment = model('Comment', commentSchema);
-
 // export the Comment model
-module.exports = Comment;
+module.exports = commentSchema;
--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema(
     {
@@ -32,8 +32,32 @@ const UserSchema = new Schema(
                 ref: 'User'
             }
         ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
     }
 );
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+})
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+}
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
 
 // create the User model using the userSchema
 const User = model('User', userSchema);
