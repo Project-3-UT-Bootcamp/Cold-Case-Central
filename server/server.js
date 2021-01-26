9050f8b5b0023a101c6d3c5acd0a5027f0e744e5
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-26 13:57:18 -0600

    notes

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+// BUILD FILE NOT CREATED YET***** 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
