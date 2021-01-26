f5f354b82954bfc567df4e898a5cd3447e87443e
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-26 11:09:08 -0600

    updated me query to include context and auth error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,12 +5,17 @@ const { signToken } = require('../utils/auth');
 // resolvers - incomplete
 const resolvers = {
   Query: {
-    me: async (parent, args) => {
-      const userData = await User.findOne({})
-      .select('-__v -password')
-      // .populate('friends')
-
-      return userData;
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('thoughts')
+          .populate('friends');
+    
+        return userData;
+      }
+    
+      throw new AuthenticationError('Not logged in');
     }
   },
   Mutation: {
