a68262d8a2b06abe758c7bb155643e5bce8a463d
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-26 14:47:47 -0600

    created single case query definition, added imports,

--- client/src/pages/SingleCase.js
+++ client/src/pages/SingleCase.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHT } from '../utils/queries';
 
-const SingleCase = props => {
+const SingleCase = (props) => {
+  const { id: caseId } = useParams();
+
   return (
     <div>
       <div>
         <p className="card-header">
-          <span>
-            Username
-          </span>{' '}
-          thought on createdAt
+          <span>Username</span> thought on createdAt
         </p>
         <div className="card-body">
           <p>Case Text</p>
