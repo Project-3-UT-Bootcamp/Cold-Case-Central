421330302563e7f014f3285f602bcdfcb99baa2b
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-26 11:56:19 -0600

    created utils folder with queries file - created comments query

---
+++ client/src/utils/queries.js
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag';
+
+export const QUERY_COMMENTS = gql`
+  query comments($username: String) {
+    comments(username: $username) {
+      _id
+      commentText
+      createdAt
+      username
+    }
+  }
+`;
