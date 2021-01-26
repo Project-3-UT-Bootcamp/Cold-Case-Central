67550b66053f62508fabfbec311b2699252a7307
catse2000 <catse2000@yahoo.com>
2021-01-25 22:18:10 -0600

    added scripts in package.json to help with installing packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
+// import Apollo hooks and modules
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
+// import components
 import Header from './components/Header';
+import Footer from './components/Footer';
+
+// import pages
+import Home from './pages/Home';
+
+const client = new ApolloClient({
+  uri: '/graphql'
+});
 
 function App() {
   return (
-    <div>
-      <main>
+    <ApolloProvider client={client}>
+      <div>
         <Header />
-      </main>
-    </div>
+          <div>
+            <Home />
+          </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
   );
 }
 
