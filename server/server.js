feb3222d71e1a560b972aa19b98450607a500592
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-26 13:56:42 -0600

    added code that checks to see if the Node environment is in production, instructs  Express.js server to serve any files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express'); // import express
-const path = require('path'); // import path (to access file and directory paths)
-const { authMiddleware } = require('./utils/auth');
+const express = require("express"); // import express
+const path = require("path"); // import path (to access file and directory paths)
+const { authMiddleware } = require("./utils/auth");
 
 const PORT = process.env.PORT || 3001; // define local port
 const app = express(); // instantiate express server
@@ -9,12 +9,24 @@ app.use(express.urlencoded({ extended: true })); // parse incoming string or arr
 app.use(express.json()); // parse incoming JSON data
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware
-  });
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // connect to server
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-})
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
