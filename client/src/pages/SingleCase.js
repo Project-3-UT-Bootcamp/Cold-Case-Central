95f483405ee3395f2a6af709030f8725b76e6aa3
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-26 14:48:08 -0600

    updated import link

--- client/src/pages/SingleCase.js
+++ client/src/pages/SingleCase.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_THOUGHT } from '../utils/queries';
+import { QUERY_CASE } from '../utils/queries';
 
 const SingleCase = (props) => {
   const { id: caseId } = useParams();
