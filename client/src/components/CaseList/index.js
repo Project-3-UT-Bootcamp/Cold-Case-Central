607010ecaba891df2a3712b5e681a3edeaba454e
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-26 12:25:27 -0600

    added caselist jsx

--- client/src/components/CaseList/index.js
+++ client/src/components/CaseList/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const CaseList = () => {
-    return (
-        <div>
-            Test Case List
-        </div>
-    )
+const CaseList = ({ cases, title }) => {
+  if (!cases.length) {
+    return <h3>No Cases Yet</h3>;
+  }
+  return (
+    <div>
+      <h3>{title}</h3>
+      {cases &&
+        cases.map(cases => (
+          <div key={cases._id} className="">
+            <p className="">
+              {cases.username}
+              case on {cases.createdAt}
+            </p>
+            <div className="">
+              <p>{cases.caseText}</p>
+              <p className="">
+                Comments: {cases.commentCount} || Click to{' '}
+                {cases.commentCount ? 'see' : 'start'} the discussion!
+              </p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default CaseList;
