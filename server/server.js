00015d634b0b1f8bc997620add7a6888fe9be83a
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-26 11:06:08 -0600

    added auth midleware method and apolloServer

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ const app = express(); // instantiate express server
 app.use(express.urlencoded({ extended: true })); // parse incoming string or array data
 app.use(express.json()); // parse incoming JSON data
 
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => req.headers
+  });
+
 // connect to server
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
