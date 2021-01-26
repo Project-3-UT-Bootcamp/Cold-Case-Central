c88849c5c46a3e78361562abd4489276642825a1
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-26 14:52:27 -0600

    loading, data vars

--- client/src/pages/SingleCase.js
+++ client/src/pages/SingleCase.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_CASE } from '../utils/queries';
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_CASE } from "../utils/queries";
 
 const SingleCase = (props) => {
   const { id: caseId } = useParams();
 
+  const { loading, data } = useQuery(QUERY_CASE, {
+    variables: { id: caseId },
+  });
+
+  const cases = data?.case || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <div>
