e7d434615fb4587fb4f239ca0f35bdab272cb28a
tarrantrl <tarrantrl@utexas.edu>
2021-01-26 20:34:12 -0600

    add user seeds

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,7 +1,7 @@
 const faker = require('faker');
 
 const db = require('../config/connection');
-// const { Case, User } = require('../models');
+const { Case, User } = require('../models');
 
 // db.once('open', async () => {
 //   await Case.deleteMany({});
@@ -35,32 +35,27 @@ const db = require('../config/connection');
 //     await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
 //   }
 
-  // create cases
-//   let createdCases = [];
-//   for (let i = 0; i < 100; i += 1) {
-//     const caseTitle = faker.lorem.words(Math.round(Math.random() * 5) + 1);
-//     const caseDescription = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-//     const caseStartDate = faker.date();
-//     const caseStatus = "Unsolved";
-//     const caseAuthor = 
-//     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-//     const { username, _id: userId } = createdUsers.ops[randomUserIndex];
 
-//     const createdCase = await Case.create({ caseDescription, username });
-
-//     const updatedUser = await User.updateOne(
-//       { _id: userId },
-//       { $push: { thoughts: createdThought._id } }
-//     );
-
-//     createdThoughts.push(createdThought);
-//   }
-const { Case } = require('../models');
+// const { Case } = require('../models');
 
 db.once('open', async () => {
   await Case.deleteMany({});
+  await User.deleteMany({});
 
   // create user data
+  const userData = [];
+
+  for (let i = 0; i < 10; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
+
+    userData.push({ username, email, password });
+  }
+
+  await User.collection.insertMany(userData);
+
+  // create case data
   const caseData = [];
 
   for (let i = 0; i < 5; i++) {
