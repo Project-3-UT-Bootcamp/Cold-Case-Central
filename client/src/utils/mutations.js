dffcea7e164628a5d5f33f52f6deda6642b9a715
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-26 20:02:59 -0600

    created add comment mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,10 @@
+export const ADD_COMMENT = gql`
+  mutation addComment($commentText: String!) {
+    addComment(commentText: $commentText) {
+      _id
+      commentText
+      createdAt
+      username
+    }
+  }
+`;
