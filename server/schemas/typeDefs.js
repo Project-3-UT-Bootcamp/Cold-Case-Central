2acef06dc612ad25d8d8801cd62e6f406aa3c186
catse2000 <catse2000@yahoo.com>
2021-01-26 23:20:04 -0600

    fixed small bug

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -55,35 +55,4 @@ const typeDefs = gql`
     }
 `;
 
-
-// // incomplete
-// const typeDefs = gql`
-//     type User {
-//         _id: ID
-//         username: String
-//         email: String
-//         friendCount: Int
-//         friends: [User]
-//     }
-
-//     type Query {
-//         me: User
-//         users: [User]
-//         user(username: String!): User
-//     }
-
-//     type Mutation {
-//         login(email: String!, password: String!): User
-//         addUser(username: String!, email: String!, password: String!): User
-//         addFriend(friendId: ID!): User
-//         addComment(commentText: String!): Comment
-//     }
-
-//     type Auth {
-//         token: ID!
-//         user: User
-//       }
-      
-// `;
-
 module.exports = typeDefs;
