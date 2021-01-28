9dc200b7fd57ac3097803d786c907f89d73c5d8c
catse2000 <catse2000@yahoo.com>
2021-01-27 23:15:08 -0600

    add styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -11,11 +11,13 @@ const Header = () => {
     return (
         <header>
             <div>
-                <Link to="/">
+                <Link to="/" className="text-decoration-none text-body">
                     <h1>COLD CASE CENTRAL</h1>
                 </Link>
                 <div className="double-border"></div>
-                <nav className="text-center">
+                <div className="d-flex flex-row justify-content-between">
+                    <span className="english-font px-3 py-2">January 28th, 2021</span>
+                <nav className="d-flex flex-row justify-content-end">
                     {Auth.loggedIn() ? (
                         <>
                             {/* <Link to="/profile">Me</Link> */}
@@ -25,11 +27,13 @@ const Header = () => {
                         </>
                     ) : (
                     <>
-                        <Link to="/login">Login</Link>
-                        <Link to="/signup">Signup</Link>
+                        <Link to="/login" className="px-3 py-2 text-uppercase">Login</Link>
+                        <Link to="/signup" className="px-3 py-2 text-uppercase">Signup</Link>
                     </>
                     )}
                 </nav>
+                </div>
+
                 <div className="double-border"></div>
             </div>
         </header>
