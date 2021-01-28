696c3e2ddd04b5d027b13cb8c7fa6337cf5fde2f
catse2000 <catse2000@yahoo.com>
2021-01-28 15:01:31 -0600

    added styling to login and signup pages

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -14,6 +14,7 @@ const Header = () => {
                 <Link to="/" className="text-decoration-none text-body">
                     <h1>COLD CASE CENTRAL</h1>
                 </Link>
+                <p className="text-center">Your one-stop shop for new cold cases, unsolved mysteries, and the theories that surround them.</p>
                 <div className="double-border"></div>
                 <div className="d-flex flex-row justify-content-between">
                     <span className="english-font px-3 py-2">January 28th, 2021</span>
